da70d59bf2db792e462037b441161a825a2bd371
0xmarWan7A <69312897+0xmarWan7A@users.noreply.github.com>
2024-08-22 19:26:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var type = new Typed(".job", {
-  strings: ["", "Mechanical Engineer", "MEP Engineer"],
+  strings: ["", "Mechanical Engineer", "MEP Engineer", "HVAC", "FireFighting", "Plumbing"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true,
